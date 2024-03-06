564268d1ab7fa860eacc4e8eddfd96079795a5b9
npx <karthik@lyrostech.com>
2024-03-06 15:55:08 +0530

    hiii

--- src/App1.js
+++ src/App1.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export default function App1(props) {
-    App1.defaultprops = {name: "cathy", age: 20};
+    App1.defaultPnrops = {name: "cathy", age: 20};
   return (
     <div>Hello{props.name},you are{props.age}</div>
-  )
+  );
 }
