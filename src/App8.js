42503763ab8981cd823916642323964a96c824d8
npx <karthik@lyrostech.com>
2024-03-09 16:48:27 +0530

    hoo

---
+++ src/App8.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {FaHome, FaBookmark, } from "react-icons/fa"
+export default function () {
+  return (
+    <div>
+        <p><FaHome />Home</p>
+        <p>post</p>
+        <p><FaBookmark />Feeds</p>
+        <p>Cards</p>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App10 from './App10';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App10 />
   </React.StrictMode>
 );
 
