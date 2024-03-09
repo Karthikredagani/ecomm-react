2a28f8991c537caf68de8450871cc9742f9a9b54
npx <karthik@lyrostech.com>
2024-03-09 18:36:13 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App6 from './App6';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App6 />
+    <App />
   </React.StrictMode>
 );
 
